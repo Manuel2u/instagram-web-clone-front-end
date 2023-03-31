957e342e326a05494e05cd58a8016e4db0d04da1
Manuel2u <edodoo011@st.ug.edu.gh>
2023-03-31 11:10:55 +0000

    still on signup --using ephraim template

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
-const appStore = require("../assets/appStore.png")
-const playStore = require("../assets/playStore.png")
-const facebook = require("../assets/facebook.png")
+import React from "react";
+import { Link } from "react-router-dom";
+const appStore = require("../assets/appStore.png");
+const playStore = require("../assets/playStore.png");
+const facebook = require("../assets/facebook.png");
 
 function SignIn() {
   return (
     <>
       <div className="login-container">
-        <div className="box-1">
-          <div className="box-1-logo">
-            <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt="Instagram" className="instagram-logo"></img>
+        <div className="box-1 flex flex-col">
+          <div className="box-1-logo flex justify-center">
+            <img
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+              alt="Instagram"
+              className="instagram-logo"
+            ></img>
           </div>
-          <div className='input-box'>
+          <div className="input-box">
             <input
+              className="mb-4"
               type="text"
-              placeholder="Phone number, username or email" />
+              placeholder="username or email"
+            />
           </div>
           <div className="input-box">
             <input type="text" placeholder="Password" />
@@ -31,19 +38,24 @@ function SignIn() {
             <span>
               <img src={facebook} alt="facebook" className="facebook-logo" />
             </span>
-            <p className='fb-link'>Log in with Facebook</p>
+            <p className="fb-link">Log in with Facebook</p>
           </div>
           <div className="forgotten-password-box">
             <p className="forgotten-password-link">Forgotten your password?</p>
           </div>
         </div>
-        <div className='box-2'>
-          <p>Don't have an account? <span className="sign-up-span">Sign Up</span></p>
+        <div className="box-2">
+          <p>
+            Don't have an account?
+            <Link to="/accounts/email/signup">
+              <span className="sign-up-span text-blue-500">Sign Up</span>
+            </Link>
+          </p>
         </div>
         <div className="get-app-box">
           <p>Get the app.</p>
         </div>
-        <div className="app-store-google-play-box">
+        <div className="app-store-google-play-box flex">
           <img src={appStore} alt="AppStore" className="app-store-image" />
           <img src={playStore} alt="PlayStore" className="google-play-image" />
         </div>
@@ -52,4 +64,4 @@ function SignIn() {
   );
 }
 
-export default SignIn
+export default SignIn;
