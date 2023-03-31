3c25446310f6eb7039e12e1bb1741ce93b77fdd4
greatnessmensah <ephraimmensah99@gmail.com>
2023-03-31 11:27:28 +0000

    pulled new changes

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,67 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const appStore = require("../assets/appStore.png");
-const playStore = require("../assets/playStore.png");
-const facebook = require("../assets/facebook.png");
+import React from 'react'
 
 function SignUp() {
   return (
-    <>
-      <div className="login-container">
-        <div className="box-1 flex flex-col">
-          <div className="box-1-logo flex justify-center">
-            <img
-              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
-              alt="Instagram"
-              className="instagram-logo"
-            ></img>
-          </div>
-          <div className="input-box">
-            <input
-              className="mb-4"
-              type="text"
-              placeholder="username or email"
-            />
-          </div>
-          <div className="input-box">
-            <input type="text" placeholder="Password" />
-          </div>
-          <div className="login-button-box">
-            <button className="login-button">Sign Up</button>
-          </div>
-          <div className="lines-box">
-            <div className="line-1"></div>
-            <div className="or-box">OR</div>
-            <div className="line-2"></div>
-          </div>
-          <div className="fb-box">
-            <span>
-              <img src={facebook} alt="facebook" className="facebook-logo" />
-            </span>
-            <p className="fb-link">Log in with Facebook</p>
-          </div>
-          <div className="forgotten-password-box">
-            <p className="forgotten-password-link">Forgotten your password?</p>
-          </div>
-        </div>
-        <div className="box-2">
-          <p>
-            Already have an account?
-            <Link to="/accounts/login">
-              <span className="sign-up-span text-blue-500"> Sign In</span>
-            </Link>
-          </p>
-        </div>
-        <div className="get-app-box">
-          <p>Get the app.</p>
-        </div>
-        <div className="app-store-google-play-box flex">
-          <img src={appStore} alt="AppStore" className="app-store-image" />
-          <img src={playStore} alt="PlayStore" className="google-play-image" />
-        </div>
-      </div>
-    </>
-  );
+    <div>
+      <h1 className='text-4xl'>Sign Up</h1>
+    </div>
+  )
 }
 
-export default SignUp;
+export default SignUp
